11ca31ac96f4f78781a18cc9d65be66014d2d607
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-09-11 04:22:24 +0530

    objectDestructuring

--- objectDestructuring.js
+++ objectDestructuring.js
@@ -1,35 +1,35 @@
-// // Destructuring Objects
-// const user = {
-//     name: 'John Doe',
-//     age: 30,
-//     job: 'Developer'
-// };
-// const { name, age, job } = user;
-// console.log(name); // John Doe
-// console.log(age); // 30
-// console.log(job); // Developer
+// Destructuring Objects
+const user = {
+    name: 'John Doe',
+    age: 30,
+    job: 'Developer'
+};
+const { name, age, job } = user;
+console.log(name); // John Doe
+console.log(age); // 30
+console.log(job); // Developer
   
 
-// // rename variables while destructuring
-// const user1 = {
-// name: 'John Doe',
-// age: 30,
-// job: 'Developer'
-// };
-// const { name: userName, age: userAge, job: userJob } = user1;
-// console.log(userName); // John Doe
-// console.log(userAge); // 30
-// console.log(userJob); // Developer
+// rename variables while destructuring
+const user1 = {
+name: 'John Doe',
+age: 30,
+job: 'Developer'
+};
+const { name: userName, age: userAge, job: userJob } = user1;
+console.log(userName); // John Doe
+console.log(userAge); // 30
+console.log(userJob); // Developer
 
 
-// // Setting Default values
-// const user3 = {
-// name1: 'John Doe',
-// age: 30
-// };
-// const { name1, job1 = 'Unemployed' } = user3;
-// console.log(name); // John Doe
-// console.log(job); // Unemployed
+// Setting Default values
+const user3 = {
+name1: 'John Doe',
+age: 30
+};
+const { name1, job1 = 'Unemployed' } = user3;
+console.log(name); // John Doe
+console.log(job); // Unemployed
 
 
 
