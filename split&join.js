4bbb786e4892732c21cfd8403f3117634ee7c31c
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-09-10 06:46:22 +0530

    Join and split

--- split&join.js
+++ split&join.js
@@ -10,3 +10,12 @@ console.log(isPalindrome(inputString)); // true
 
 inputString = "hello";
 console.log(isPalindrome(inputString)); // false
+
+
+
+function func() { 
+    var a = [ 1, 2, 3, 4, 5, 6 ]; 
+    console.log(a.join()); 
+    console.log(a.join('')); 
+}  
+func(); 
